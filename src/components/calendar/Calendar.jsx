01854edb9725d5d6249b18acb17cea1bd44a0ecf
Quang Nguyen Huynh Minh <124662156+quang21122@users.noreply.github.com>
2024-10-25 23:18:16 +0700

    Calendar
    Fix lại vài cái

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -44,7 +44,7 @@ export const months = [
     'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
 ]
 
-const Calender = () => {
+const Calendar = () => {
     const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     const [today, setToday] = React.useState(dayjs());
     const [selectedDate, setSelectedDate] = React.useState(dayjs());
@@ -83,4 +83,4 @@ const Calender = () => {
     );
 };
 
-export default Calender
+export default Calendar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
+import Calendar from "../components/calendar/Calendar";
+
 function Home() {
   return (
-    <div className="flex justify-center h-screen items-center">
-      <h1 className="text-red-500 text-6xl">Hello World</h1>
-    </div>
+    <Calendar />
   );
 }
 
