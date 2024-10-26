adfe65780db4fbc0637a47b47ffb8898f3a814e8
ntpd164 <duongngo1616@gmail.com>
2024-10-26 11:51:22 +0700

    Use localStorage for data retrieval to reduce API requests

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import Calendar from "../components/calendar/Calendar";
-import FinishedMatches from "../components/Matches/FinishedMatches";
+import FinishedMatches from "../components/Matches/FinishedMatches-v2";
 
 function Home() {
   return (
     <div className="grid grid-cols-3">
       <Calendar />
-      <FinishedMatches date={'2024-10-20'} />
+      {/* <FinishedMatches date={'2024-10-20'} /> */}
+      <FinishedMatches />
     </div>
   );
 }
