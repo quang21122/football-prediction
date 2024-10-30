23c68121df9e55712c92e88ce1397eb23e23d806
ntpd164 <duongngo1616@gmail.com>
2024-10-30 15:03:40 +0700

    Add substitute players

--- src/components/Matches/MatchDetails.jsx
+++ src/components/Matches/MatchDetails.jsx
@@ -3,7 +3,8 @@ import { useState, useEffect } from 'react';
 
 function MatchDetails({ selectedMatch }) {
     const [lineups, setLineups] = useState([]);
-    const [playersData, setPlayersData] = useState({}); // lưu thông tin cầu thủ
+    const [playersData, setPlayersData] = useState({});
+    const [selectedDetails, setSelectedDetails] = useState('lineups');
 
     useEffect(() => {
         const fetchLineups = async () => {
@@ -22,12 +23,10 @@ function MatchDetails({ selectedMatch }) {
                 const lineupData = await lineupResponse.json();
                 setLineups(lineupData.response);
                 localStorage.setItem('lineups', JSON.stringify(lineupData.response));
-                console.log('lineupData', lineupData.response);
 
                 const playersResponse = await fetch(`https://v3.football.api-sports.io/fixtures/players?fixture=${selectedMatch.fixture.id}`, requestOptions);
                 const playersData = await playersResponse.json();
 
-                // Tạo dictionary để dễ dàng lấy thông tin ảnh cầu thủ dựa trên ID
                 const playerPhotos = playersData.response.reduce((acc, team) => {
                     team.players.forEach(player => {
                         acc[player.player.id] = player.player.photo;
@@ -37,7 +36,6 @@ function MatchDetails({ selectedMatch }) {
 
                 setPlayersData(playerPhotos);
                 localStorage.setItem('playersData', JSON.stringify(playerPhotos));
-                console.log('playersData', playerPhotos);
             } catch (error) {
                 console.error('Error fetching lineups or players:', error);
             }
@@ -92,13 +90,25 @@ function MatchDetails({ selectedMatch }) {
                     <span>{convertDate(selectedMatch.fixture.date).formattedDate}</span>
                 </div>
                 <div className='gap-x-4 flex justify-around mt-10 text-2xl'>
-                    <span>Đội hình</span>
-                    <span className='text-wrap text-center w-24'>Lịch sử đối đầu</span>
-                    <span className='text-wrap text-center w-32'>Các trận gần đây</span>
+                    <span 
+                        className={`mt-auto cursor-pointer py-4 px-8 ${selectedDetails === 'lineups' ? 'border-b-4 border-green-400' : ''}`}
+                        onClick={() => setSelectedDetails('lineups')}>
+                            Đội hình
+                    </span>
+                    <span 
+                        className={`text-wrap text-center cursor-pointer w-24 ${selectedDetails === 'h2h' ? 'border-b-4 border-green-400' : ''}` }
+                        onClick={() => setSelectedDetails('h2h')}>
+                            Lịch sử đối đầu
+                    </span>
+                    <span 
+                        className={`text-wrap text-center cursor-pointer w-32 ${selectedDetails === 'recent' ? 'border-b-4 border-green-400' : ''}` }
+                        onClick={() => setSelectedDetails('recent')}>
+                        Các trận gần đây
+                    </span>
                 </div>
             </div>
-            {lineups.length > 0 && (
-                <div className="bg-green-50 p-8 mt-12 rounded-lg">
+            {selectedDetails === 'lineups' && lineups.length > 0 && (
+                <div className="bg-green-50 p-8 m-6 mt-12 rounded-[3rem]">
                     <h3 className="text-3xl font-bold text-green-700 mb-6">Đội hình ra sân</h3>
                     <div className="flex justify-around">
                         {lineups.map((teamLineup, index) => (
@@ -106,6 +116,9 @@ function MatchDetails({ selectedMatch }) {
                                 <h4 className="text-2xl font-bold text-green-600 mb-4">{teamLineup.team.name}</h4>
                                 <img src={teamLineup.team.logo} alt={teamLineup.team.name} className="w-20 h-20 mb-4" />
                                 <p className="text-xl font-bold text-gray-700 mb-2">Huấn luyện viên: {teamLineup.coach.name}</p>
+                                    
+                                {/* Starting XI */}
+                                <h5 className="text-xl font-semibold text-green-600 my-4">Đội hình xuất phát</h5>
                                 <ul className="space-y-4">
                                     {teamLineup.startXI.map((player, playerIndex) => (
                                         <li key={playerIndex} className="flex items-center space-x-4">
@@ -116,6 +129,22 @@ function MatchDetails({ selectedMatch }) {
                                             />
                                             <span className="font-semibold">{player.player.number}</span>
                                             <span>{player.player.name}</span>
+                                        </li>
+                                    ))}
+                                </ul>
+
+                                {/* Substitutes */}
+                                <h5 className="text-xl font-semibold text-green-600 my-4">Cầu thủ dự bị</h5>
+                                <ul className="space-y-4">
+                                    {teamLineup.substitutes.map((sub, subIndex) => (
+                                        <li key={subIndex} className="flex items-center space-x-4">
+                                            <img
+                                                src={playersData[sub.player.id] || 'https://via.placeholder.com/50'}
+                                                alt={sub.player.name}
+                                                className="w-12 h-12 rounded-full"
+                                            />
+                                            <span className="font-semibold">{sub.player.number}</span>
+                                            <span>{sub.player.name}</span>
                                         </li>
                                     ))}
                                 </ul>
