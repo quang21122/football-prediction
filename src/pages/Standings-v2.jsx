6f7a8bca17d96ab4e333578f2a99679453c58af2
Quang Nguyen Huynh Minh <124662156+quang21122@users.noreply.github.com>
2024-11-11 19:31:42 +0700

    Standing

--- src/pages/Standings-v2.jsx
+++ src/pages/Standings-v2.jsx
@@ -1,37 +1,121 @@
 import { useEffect, useState } from "react";
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+// import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+// import { useNavigate } from "react-router-dom";
+// import PropTypes from 'prop-types';
 
 const Standings = () => {
   const [standings, setStandings] = useState([]);
+  const [leagueName, setLeagueName] = useState("");
+  const [displayLimit, setDisplayLimit] = useState(10);
+
+  const leaderboardArr = [' ', ' ', 'ST', 'T', 'H', 'B', 'BT', 'BB', '+/-', 'Đ'];
 
   useEffect(() => {
     const savedStandings = JSON.parse(localStorage.getItem("standingsData"));
     console.log("Standings data:", savedStandings);
     if (savedStandings && savedStandings[0].league.standings[0]) {
       setStandings(savedStandings[0].league.standings[0]);
+      setLeagueName(savedStandings[0].league.name);
     }
   }, []);
 
   return (
-    <div>
-      <h1>EPL Standings 2022</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Position</th>
-            <th>Team</th>
-            <th>Points</th>
-          </tr>
-        </thead>
-        <tbody>
-          {standings.map((team, index) => (
-            <tr key={index}>
-              <td>{team.rank}</td>
-              <td>{team.team.name}</td>
-              <td>{team.points}</td>
+    <div className="grid grid-cols-[3fr_7fr] py-10 gap-x-20 max-w-[120rem] mx-auto border border-x-2 border-gray-200">
+      <div className="grid grid-rows-2 w-[95%]">
+      </div>
+      <div className="">
+        <div
+          className="py-6 rounded-[2.5rem] bg-gradient-to-r from-primary to-primary-dark
+      text-white text-5xl font-bold text-center flex flex-row justify-between items-center px-10 w-[94%]"
+        >
+          <GrFormPrevious className="text-5xl hover:cursor-pointer" />
+          {leagueName}
+          <GrFormNext className="text-5xl hover:cursor-pointer" />
+        </div>
+        <table className="w-[94%] mt-10 shadow-sm">
+          <thead>
+            <tr>
+              {leaderboardArr.map((item, index) => (
+                <th
+                  key={index}
+                  className="text-center text-3xl bg-red-500 py-3 text-white"
+                >
+                  {item}
+                </th>
+              ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {standings.slice(0, displayLimit).map((team, index) => (
+              <tr
+                key={index}
+                className={`${
+                  index % 2 === 0 ? "" : "bg-gray-100"
+                } text-3xl text-center`}
+              >
+                <td className="py-5 px-6">{team.rank}</td>
+                <td className="py-5 flex flex-row justify-start items-center">
+                  <img
+                    src={team.team.logo}
+                    alt={team.team.name}
+                    className="w-12 h-12 mr-6"
+                  />
+                  {team.team.name}
+                </td>
+                <td className="py-5 px-6">{team.all.played}</td>
+                <td className="py-5 px-6">{team.all.win}</td>
+                <td className="py-5 px-6">{team.all.draw}</td>
+                <td className="py-5 px-6">{team.all.lose}</td>
+                <td className="py-5 px-6">{team.all.goals.for}</td>
+                <td className="py-5 px-6">{team.all.goals.against}</td>
+                <td className="py-5 px-6">
+                  {team.all.goals.for - team.all.goals.against >= 0
+                    ? "+" + (team.all.goals.for - team.all.goals.against)
+                    : "-" + (team.all.goals.against - team.all.goals.for)}
+                </td>
+                <td className="py-7 px-6">{team.points}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className="grid grid-cols-4 text-3xl text-red-500 ml-40 mt-6">
+          <div className="flex flex-col">
+            <p>ST: Số trận</p>
+            <p>BT: Bàn thắng</p>
+          </div>
+          <div className="flex flex-col">
+            <p>T: Thắng</p>
+            <p>BB: Bàn bại</p>
+          </div>
+          <div className="flex flex-col">
+            <p>H: Hòa</p>
+            <p>+/-: Hiệu số</p>
+          </div>
+          <div className="flex flex-col">
+            <p>B: Bại</p>
+            <p>Đ: Điểm</p>
+          </div>
+        </div>
+        <div className="flex justify-center items-center">
+          {standings.length > displayLimit && (
+            <button
+              onClick={() => setDisplayLimit(displayLimit + 10)}
+              className="text-3xl border border-red-300 py-6 px-8 mt-10 rounded-full hover:bg-red-500 hover:text-white"
+            >
+              Xem thêm
+            </button>
+          )}
+          {standings.length <= displayLimit && (
+            <button
+              onClick={() => setDisplayLimit(10)}
+              className="text-3xl border border-red-300 py-6 px-8 mt-10 rounded-full hover:bg-red-500 hover:text-white"
+            >
+              Rút gọn
+            </button>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
