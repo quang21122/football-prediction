8424f800bfdcc0eafc2b2d67eff91a4267da6879
puiscoding <22127446@student.hcmus.edu.vn>
2024-12-09 22:56:03 +0700

    fix signin singup

--- src/components/signInForm/signInForm.jsx
+++ src/components/signInForm/signInForm.jsx
@@ -1,9 +1,9 @@
 import { useNavigate } from 'react-router-dom'
 function UserInput({label, field, placeHolder}){
     return (
-        <div className="flex flex-col text-lg">
+        <div className="flex flex-col text-xl w-[28rem]">
             <label className="font-semibold text-gray-700 mb-3">{label}</label>
-            <input className ="w-96 h-16 bg-gray-100 text-base p-2 rounded-md outline-none mb-8"
+            <input className ="h-16 bg-gray-100 p-2 rounded-md outline-none mb-8"
             type={field}
             placeholder={placeHolder}/>
         </div>
@@ -19,14 +19,14 @@ const SignInForm = () =>
     };
     
     return (
-            <div className="flex flex-col min-w-[300px] w-1/4 h-[38rem] items-center bg-white rounded-3xl">
-                <div className="text-2xl text-red-400 mt-12 mb-2">Welcome back to</div>
-                <div className="text-3xl text-red-400 font-bold mb-10">Football Prediction</div>
+            <div className="flex flex-col min-w-[360px] w-1/4 h-[42rem] items-center bg-white rounded-3xl">
+                <div className="text-4xl text-red-400 mt-12 mb-6">Welcome back to</div>
+                <div className="text-4xl text-red-400 font-bold mb-10">Football Prediction</div>
                 <UserInput label="Email" field="email" placeHolder="example.email@gmail.com" />
                 <UserInput label="Mật khẩu" field="password" placeHolder="Mật khẩu phải chứa ít nhất 8 kí tự" />
                 <button onClick={handleNavigateHome}
-                className=" w-96 h-16 bg-red-400 hover:bg-red-500 text-3xl text-white font-bold rounded-md outline-none mt-6">Đăng nhập</button>
-                <div className="min-w-[300px] text-red-500 text-center text-base mt-8" >Chưa có tài khoản?  
+                className=" w-[28rem] h-16 bg-red-400 hover:bg-red-500 text-3xl text-white font-bold rounded-md outline-none mt-6">Đăng nhập</button>
+                <div className="min-w-[300px] text-red-500 text-center text-2xl mt-8" >Chưa có tài khoản?  
                     <a href="/signup" className = "p-2 font-semibold hover:text-red-700 text-red-500">Đăng ký</a>
                 </div>
             </div>
@@ -40,15 +40,15 @@ const SignUpForm = () =>
         navigate('/');
     };
     return(
-        <div className="flex flex-col w-1/4 min-w-[300px] h-[48rem] items-center bg-white rounded-3xl">
-            <div className="text-2xl text-red-400 mt-12 mb-2">Welcome to</div>
-            <div className="text-3xl text-red-400 font-bold mb-10">Football Prediction</div>
+        <div className="flex flex-col w-1/4 min-w-[300px] h-[50rem] items-center bg-white rounded-3xl">
+            <div className="text-4xl text-red-400 mt-12 mb-6">Welcome to</div>
+            <div className="text-4xl text-red-400 font-bold mb-10">Football Prediction</div>
             <UserInput label="Email" field="email" placeHolder="example.email@gmail.com" />
             <UserInput label="Mật khẩu" field="password" placeHolder="Mật khẩu phải chứa ít nhất 8 kí tự" />
             <UserInput label="Xác nhận mật khẩu" field="password" placeHolder="Mật khẩu phải chứa ít nhất 8 kí tự" />
             <button onClick={handleNavigateHome}
-            className="w-96 h-16 bg-red-400 hover:bg-red-500 text-3xl text-white font-bold rounded-md outline-none my-6 mb-12 ">Đăng ký</button>
-            <div className="min-w-[300px] text-center text-red-500 mb-6 text-base" >Đã có tài khoản?  
+            className="w-[28rem] h-16 bg-red-400 hover:bg-red-500 text-3xl text-white font-bold rounded-md outline-none my-6 mb-12 ">Đăng ký</button>
+            <div className="min-w-[300px] text-center text-red-500 mb-6 text-2xl" >Đã có tài khoản?  
                 <a href="/signin" className = "p-2 font-semibold hover:text-red-700 text-red-500">Đăng nhập</a>
             </div>
         </div>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -5,12 +5,12 @@ import googleIcon from "../assets/Google.png";
 function SignUp(){
     return(
         <div className="bg-width h-screen">
-            <div className="max-w-[120rem] pt-36 mx-auto h-screen flex flex-col items-center bg-red-200">
+            <div className="max-w-[160rem] pt-36 mx-auto h-screen flex flex-col items-center bg-red-200">
                 <SignUpForm/>
                 <div className="w-1/4 text-left text-base text-gray-700 mt-8 mb-4">hoặc</div>
                 <button className="flex hover:bg-gray-100 justify-start items-center w-1/4 h-[5rem] text-gray-700 bg-white rounded-3xl"> 
                     <img className="w-12 h-12 ml-16" src={googleIcon} alt="Google Icon"  />
-                    <div className="ml-10 mr-auto text-2xl ">Đăng ký bằng Google</div>
+                    <div className="ml-10 mr-auto text-3xl ">Đăng ký bằng Google</div>
                 </button>
             </div>
         </div>
